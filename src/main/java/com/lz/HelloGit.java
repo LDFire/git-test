12683672c5feee9130ca25260ed383d8ad13d660
LDFire <116954387+LDFire@users.noreply.github.com>
2023-09-07 16:39:17 +0800

    Update HelloGit.java

--- src/main/java/com/lz/HelloGit.java
+++ src/main/java/com/lz/HelloGit.java
@@ -6,6 +6,6 @@ public class HelloGit {
         System.out.println("hello github");
         System.out.println("hello git3");
         System.out.println("hello git4");
-        System.out.println("master test");
+        System.out.println("test");
     }
 }
